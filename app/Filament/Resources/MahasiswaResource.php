117d86400e6740a8d1e25e368ccb34fa3aaa80da
maikel <maikelkhaq@gmail.com>
2023-07-07 19:33:49 +0700

    first commit

---
+++ app/Filament/Resources/MahasiswaResource.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use Filament\Forms;
+use Filament\Tables;
+use App\Models\Mahasiswa;
+use Filament\Resources\Form;
+use Filament\Resources\Table;
+use Filament\Resources\Resource;
+use Filament\Forms\Components\Card;
+use Filament\Forms\Components\Select;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Forms\Components\TextInput;
+use Illuminate\Database\Eloquent\Builder;
+use App\Filament\Resources\MahasiswaResource\Pages;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use App\Filament\Resources\MahasiswaResource\RelationManagers;
+
+class MahasiswaResource extends Resource
+{
+    protected static ?string $model = Mahasiswa::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-collection';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Card::make()
+                    ->schema([
+                        TextInput::make('nim')->required()->unique(),
+                        TextInput::make('nama')->required(),
+                            ]),
+                        ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                TextColumn::make('nim')->sortable()->searchable(),
+                TextColumn::make('nama')->sortable()->searchable(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+                Tables\Actions\DeleteAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+    
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+    
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListMahasiswas::route('/'),
+            'create' => Pages\CreateMahasiswa::route('/create'),
+            'edit' => Pages\EditMahasiswa::route('/{record}/edit'),
+        ];
+    }    
+}
